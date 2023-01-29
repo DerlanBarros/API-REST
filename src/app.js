03361e22e9062b569a76d71b79479586f4720078
DerlanBarros <derlanbarros2@gmail.com>
2023-01-29 17:51:22 -0300

    Implementação de instruções sql

--- src/app.js
+++ src/app.js
@@ -1,73 +1,82 @@
 import express from "express";
+import conexao from "../infra/conexao.js";
 const app = express();
 
 // indicar para o express ler body com json()
 app.use(express.json());
 
-// mock de dados
-const selecoes = [
-  {
-    id: 1,
-    time: "Brasil",
-    grupo: "G",
-  },
-  {
-    id: 2,
-    time: "Suiça",
-    grupo: "G",
-  },
-  {
-    id: 3,
-    time: "Camarões",
-    grupo: "G",
-  },
-  {
-    id: 4,
-    time: "Sérvia",
-    grupo: "G",
-  },
-];
-
-// função para fazer busca por id
-function buscarSelecaoPorId(id) {
-  return selecoes.filter((selecao) => selecao.id == id);
-}
-
-// função para buscar index da seleção
-function buscarIndexSelecao(id) {
-  return selecoes.findIndex((selecao) => selecao.id == id);
-}
+// ROTAS
 
 // Criando uma rota para seleção
 app.get("/selecoes", (req, res) => {
-  res.status(200).send(selecoes);
+  // Instruções sql
+  const sql = "SELECT * FROM tbselecoes;";
+  conexao.query(sql, (erro, result) => {
+    if (erro) {
+      res.status(404).json({ erro: erro });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 //Pesquisar seleção por id
 app.get("/selecoes/:id", (req, res) => {
-  let index = req.params.id;
-  res.status(200).send(buscarSelecaoPorId(index));
+  let id = req.params.id;
+  // Instrução sql
+  const sql = `SELECT * FROM tbselecoes WHERE id=?;`;
+  conexao.query(sql, id, (erro, result) => {
+    const row = result[0];
+    if (erro) {
+      res.status(404).json({
+        erro: erro,
+      });
+    } else {
+      res.status(200).json(row);
+    }
+  });
 });
 
-// Adicionando conteudo ao array
+// Adicionando conteudo ao banco de dados
 app.post("/selecoes", (req, res) => {
-  selecoes.push(req.body);
-  res.status(201).send("Seleção cadastrado com sucesso!");
+  const selecao = req.body.selecao;
+  const grupo = req.body.grupo;
+  // Instrução sql
+  const sql = `INSERT INTO tbselecoes(selecao, grupo) values (?,?)`;
+  conexao.query(sql, [selecao, grupo], (erro, result) => {
+    if (erro) {
+      res.status(404).json({ erro: erro });
+    } else {
+      res.status(201).json(result);
+    }
+  });
 });
 
 // Apagar seleçao por id
 app.delete("/selecoes/:id", (req, res) => {
-  let index = buscarIndexSelecao(req.params.id);
-  selecoes.splice(index, 1);
-  res.send("Seleção excluída com sucesso!");
+  let id = req.params.id;
+  const sql = "DELETE FROM tbselecoes WHERE id=?";
+  conexao.query(sql, id, (erro, result) => {
+    if (erro) {
+      res.status(404).json({ erro: erro });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 //Atualiza selecao
 app.put("/selecoes/:id", (req, res) => {
-  let index = buscarIndexSelecao(req.params.id);
-  selecoes[index].time = req.body.time;
-  selecoes[index].grupo = req.body.grupo;
-  res.json(selecoes);
+  const id = req.params.id;
+  const selecao = req.body;
+  const sql = `UPDATE tbselecoes SET ? WHERE id = ${id}`;
+  conexao.query(sql, selecao, (erro, result) => {
+    if (erro) {
+      res.status(404).json({ erro: erro });
+    } else {
+      res.status(200).json(result);
+    }
+  });
 });
 
 export default app;
